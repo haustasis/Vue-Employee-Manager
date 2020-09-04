c0c98656b2e1305e6c9f129a60f73408c5cb8585
TARISO <toni.ariso@gmail.com>
2020-09-04 08:18:02 +0200

    firebase config

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyA_OW59Fb5JxaOvg8xHcGfFlJcYJroJsC4",
+  authDomain: "vuefs-prod-43136.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-43136.firebaseio.com",
+  projectId: "vuefs-prod-43136",
+  storageBucket: "vuefs-prod-43136.appspot.com",
+  messagingSenderId: "389058159785",
+  appId: "1:389058159785:web:be91d55d2c48d92bb835b8",
+  measurementId: "G-QNHQ3ZSS0Z"
+};
